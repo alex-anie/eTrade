26d89d7cc6757e464851828543d7ca016dd9e17b
alex-anie <ocxigin@gmail.com>
2025-03-21 21:06:57 +0100

    expo CLI install

--- cypress/e2e/hello.cy.ts
+++ cypress/e2e/hello.cy.ts
@@ -1,6 +1,5 @@
 
 /// <reference types="cypress" />
-
 describe('select the hero text from the hero overlay image', ()=>{
   it('Select and assert if the hero text exits', () => {
     cy.visit('/')
